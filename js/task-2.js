c544e1ac2fb2900c904c13615c530c01cf0543ea
Anton-Vovk <mr.vovk.anton@gmail.com>
2020-12-05 16:42:38 +0200

    js-6

--- js/task-2.js
+++ js/task-2.js
@@ -4,33 +4,30 @@
 // и цену гравировки одного слова, и возвращающую цену гравировки
 // всех слов в строке.
 
-// const calculateEngravingPrice = function(message, pricePerWord) {
-  
+const calculateEngravingPrice = function (message, pricePerWord) {
+  const result = message.split(' ').length * pricePerWord;
+  return result;
+};
 
 
-// };
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-// console.log(
-//   calculateEngravingPrice(
-//     'Proin sociis natoque et magnis parturient montes mus',
-//     10,
-//   ),
-// ); // 80
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
 
-// console.log(
-//   calculateEngravingPrice(
-//     'Proin sociis natoque et magnis parturient montes mus',
-//     20,
-//   ),
-// ); // 160
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
 
-// console.log(
-//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
-// ); // 240
-
-// console.log(
-//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
-// ); // 120
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
